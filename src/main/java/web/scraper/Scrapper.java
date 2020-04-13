ed030ae91d30258e895e8ce469c59ec8fa55a0d4
Gowtham Venkatrao <gowtham.venkatrao@gmail.com>
2020-04-13 15:20:02 -0500

    updates

--- src/main/java/web/scraper/Scrapper.java
+++ src/main/java/web/scraper/Scrapper.java
@@ -1,59 +1,74 @@
 package web.scraper;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
-import java.time.Duration;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Scrapper {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver_2_29.exe");
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--headless");
 
-        WebDriver driver = new ChromeDriver();
+        Charset utf8 = StandardCharsets.UTF_8;
+        WebDriver driver = new ChromeDriver(option);
 
         try {
             driver.get("https://covid19-scoreboard.unacast.com/");
             Thread.sleep(5000);
 
+            //get county metrics
+            //step1: click on counties
             WebElement countyTag = driver.findElement(By.linkText("Counties"));
             countyTag.findElement(By.xpath("./..")).click();
             Thread.sleep(5000);
-            
-            WebElement e1 = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
 
-            //Step 2: get all child elemnets of this `div`. this will only give 1st level children
-            List<WebElement> counties = e1.findElements(By.xpath("./div"));
+            WebElement countyElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
+            List counties = getElements(countyElement);
+            Files.write(Paths.get("src/output/county.csv"), counties, utf8);
 
-            System.out.println("number of counties:" + counties.size());
-            List<String> output = new ArrayList<>();
-            //Step 3: loop through each `div`. each item contains county level details.
-            for (WebElement county : counties) {
-                String[] countyDesc = county.getText().split("\n");
-                String countyName = countyDesc[0];
-                String countyRank = countyDesc[1];
+            //get state level metrics
+            WebElement stateTag = driver.findElement(By.linkText("States"));
+            stateTag.findElement(By.xpath("./..")).click();
+            Thread.sleep(5000);
 
-                //Step 4: get the description for the county.
-                List<WebElement> countyProperties = county.findElements(By.xpath("./div/div[2]"));
-                String desc = countyProperties.get(0).getAttribute("title");
-                output.add(countyName + "," + countyRank + "," + desc);
+            WebElement stateElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
+            List states = getElements(stateElement);
+            Files.write(Paths.get("src/output/states.csv"), states, utf8);
 
-            }
-            output.forEach(System.out::println);
-        } catch (InterruptedException e) {
+
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         } finally {
             driver.quit();
         }
+    }
+
+    public static List getElements(WebElement element) {
+        List<WebElement> childElements = element.findElements(By.xpath("./div"));
+        System.out.println("Size of Child Elements" + childElements.size());
+
+        return childElements.stream().map(e -> {
+            String[] countyDesc = e.getText().split("\n");
+            String countyName = countyDesc[0];
+            String countyRank = countyDesc[1];
+
+            //Step 4: get the description for the county.
+            List<WebElement> countyProperties = e.findElements(By.xpath("./div/div[2]"));
+            String desc = countyProperties.get(0).getAttribute("title");
+            return countyName + "," + countyRank + "," + desc;
+        }).collect(Collectors.toList());
     }
 }
