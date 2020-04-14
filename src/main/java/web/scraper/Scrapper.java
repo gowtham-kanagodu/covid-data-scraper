e8d8519cffe6a15ab9a9c92ab6dda10bc866da84
Gowtham Venkatrao <gowtham.venkatrao@gmail.com>
2020-04-13 22:11:32 -0500

    enhancements to add state name along with county

--- src/main/java/web/scraper/Scrapper.java
+++ src/main/java/web/scraper/Scrapper.java
@@ -11,42 +11,32 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.lang.Thread.sleep;
 
 public class Scrapper {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver_2_29.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver_80.exe");
         ChromeOptions option = new ChromeOptions();
         option.addArguments("--headless");
 
         Charset utf8 = StandardCharsets.UTF_8;
-        WebDriver driver = new ChromeDriver(option);
+        WebDriver driver = new ChromeDriver();
 
         try {
             driver.get("https://covid19-scoreboard.unacast.com/");
-            Thread.sleep(5000);
-
-            //get county metrics
-            //step1: click on counties
-            WebElement countyTag = driver.findElement(By.linkText("Counties"));
-            countyTag.findElement(By.xpath("./..")).click();
-            Thread.sleep(5000);
-
-            WebElement countyElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
-            List counties = getElements(countyElement);
-            Files.write(Paths.get("src/output/county.csv"), counties, utf8);
-
-            //get state level metrics
-            WebElement stateTag = driver.findElement(By.linkText("States"));
-            stateTag.findElement(By.xpath("./..")).click();
-            Thread.sleep(5000);
+            sleep(5000);
+            driver.manage().window().fullscreen();
+            sleep(2000);
 
             WebElement stateElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
-            List states = getElements(stateElement);
-            Files.write(Paths.get("src/output/states.csv"), states, utf8);
+            List<Map> output = getElements(stateElement, driver);
+
+            Files.write(Paths.get("src/output/states.csv"), output.get(0).values(), utf8);
+            Files.write(Paths.get("src/output/counties.csv"), output.get(0).values(), utf8);
 
 
         } catch (InterruptedException | IOException e) {
@@ -56,19 +46,58 @@ public class Scrapper {
         }
     }
 
-    public static List getElements(WebElement element) {
-        List<WebElement> childElements = element.findElements(By.xpath("./div"));
-        System.out.println("Size of Child Elements" + childElements.size());
+    public static List getElements(WebElement element, WebDriver driver) throws InterruptedException {
+        Map states = new HashMap<String, String>();
+        Map counties = new HashMap<String, String>();
 
-        return childElements.stream().map(e -> {
-            String[] countyDesc = e.getText().split("\n");
-            String countyName = countyDesc[0];
-            String countyRank = countyDesc[1];
+        //get all state names
+        WebElement stateNameElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
+        List<WebElement> stateNames = stateNameElement.findElements(By.xpath("./div"));
+        List<String> statesList = new ArrayList<String>();
+        for (WebElement st : stateNames) {
+            String stateName = st.getText().split("\n")[0].trim();
+            System.out.println("stateName -> " + stateName);
+            statesList.add(stateName);
+        }
+        System.out.println("Total states -> " + statesList.size());
+        //loop through state by state.
+        for (String state : statesList) {
+            System.out.println("Processing - " + state);
 
-            //Step 4: get the description for the county.
-            List<WebElement> countyProperties = e.findElements(By.xpath("./div/div[2]"));
-            String desc = countyProperties.get(0).getAttribute("title");
-            return countyName + "," + countyRank + "," + desc;
-        }).collect(Collectors.toList());
+            WebElement stateElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
+            List<WebElement> oneState = stateElement.findElements(By.xpath("//*[text()='" + state + "']"));
+
+            String[] desc = oneState.get(0).getText().split("\n");
+            String stateName = desc[0];
+            String stateRank = desc[1];
+
+            System.out.println("*******" + stateName);
+
+            List<WebElement> countyProperties = oneState.get(0).findElements(By.xpath("./div/div[2]"));
+            String stateDesc = countyProperties.get(0).getAttribute("title");
+            System.out.println("*******" + stateDesc);
+
+            states.put(stateName, stateRank + "," + stateDesc);
+
+            countyProperties.get(0).click();
+            sleep(5000);
+
+            WebElement countyElement = driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[1]/div[3]/div[2]"));
+            List<WebElement> countyElements = countyElement.findElements(By.xpath("./div"));
+            System.out.println("Number Of Counties" + countyElements.size());
+
+            for (WebElement ee : countyElements) {
+                String[] countyDesc = ee.getText().split("\n");
+                String countyName = countyDesc[0];
+                String countyRank = countyDesc[1];
+                counties.put(stateName, stateName + "," + countyName + "," + countyRank + "," + countyDesc);
+                System.out.println(stateName + " -> " + countyName);
+            }
+
+            //go back to state view
+            driver.findElement(By.xpath("//*[@id=\"root\"]/main/div[2]/div[2]/div[1]/div/div[1]")).click();
+            sleep(5000);
+        }
+        return Arrays.asList(states, counties);
     }
 }
